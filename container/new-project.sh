5c96b576f508d92a8716638eaa355ed8fe09b9d3
Uli Hitzel <uli.hitzel@gmail.com>
2019-02-26 12:57:46 +0800

    container image

--- container/new-project.sh
+++ container/new-project.sh
@@ -5,7 +5,6 @@ npm install -save-dev highlight.js@9.14.2
 npm install --no-optional
 
 # Remove localhost access restriction
-
 cd conf
 cp default.js default.js.bak
 sed -i "/'localhost/d" default.js
